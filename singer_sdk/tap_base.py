50ec752cd7fe620558b0fd4eb8516517632e794e
Dan Norman <buzzcutnorman@gmail.com>
2024-03-05 09:15:11 -0800

    added a context manager to reuse Parallel workers

--- singer_sdk/tap_base.py
+++ singer_sdk/tap_base.py
@@ -480,8 +480,12 @@ class Tap(PluginBase, SingerWriter, metaclass=abc.ABCMeta):
         def _sync_one(stream: Stream) -> None:
             self.sync_one(stream)
 
-        with parallel_config(backend="loky", prefer="processes", n_jobs=-2):
-            Parallel()(delayed(_sync_one)(stream) for stream in self.streams.values())
+        with parallel_config(
+            backend="loky",
+            prefer="processes",
+            n_jobs=-2,
+        ), Parallel() as parallel:
+            parallel(delayed(_sync_one)(stream) for stream in self.streams.values())
 
         # this second loop is needed for all streams to print out their costs
         # including child streams which are otherwise skipped in the loop above
